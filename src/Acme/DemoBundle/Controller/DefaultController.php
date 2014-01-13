b12c8dfca36db5b2a8e5601b0e0276ad2222e23f
Ryan Weaver <ryan@thatsquality.com>
2014-01-13 07:38:26 -0600

    Basic security setup

--- src/Acme/DemoBundle/Controller/DefaultController.php
+++ src/Acme/DemoBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Acme\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -12,4 +14,21 @@ class DefaultController extends Controller
             'rightNow' => new \DateTime('now')
         ));
     }
+
+    public function loginAction(Request $request)
+     {
+         $session = $request->getSession();
+
+         $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+         $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+
+         return $this->render(
+             'AcmeDemoBundle:Default:login.html.twig',
+             array(
+                 // last username entered by the user
+                 'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                 'error'         => $error,
+             )
+         );
+     }
 }
