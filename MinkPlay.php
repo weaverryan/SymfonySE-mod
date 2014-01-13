0341a60e1608cf5caaa854218d2aba95baba001b
Ryan Weaver <ryan@thatsquality.com>
2014-01-12 21:49:58 -0600

    Adding starter MinkPlay file

---
+++ MinkPlay.php
@@ -0,0 +1,34 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Behat\Mink\Driver\GoutteDriver;
+
+$url = 'http://localhost:9000';
+
+/**
+ * Important object #1: Driver
+ */
+
+$driver = new GoutteDriver();
+
+/**
+ * Important object #2: Session
+ */
+$session = new Session($driver);
+$session->start();
+
+$session->visit($url);
+
+/**
+ * Important object #3: Page (DocumentElement)
+ */
+
+$page = $session->getPage();
+
+/**
+ * Important object #4: NodeElement
+ */
+
+$h1Node = $page->find('css', 'h1');
+var_dump($h1Node);
